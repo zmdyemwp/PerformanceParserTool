6fc3fde5299c27c5f681cd9c4e918dee55a65dd7
MinSMChien <MinSMChien@fih-foxconn.com>
2017-09-14 21:49:21 +0800

    New Version v1.0.1

--- src/com/fihtdc/PerformanceParser/utils/Const.java
+++ src/com/fihtdc/PerformanceParser/utils/Const.java
@@ -12,7 +12,7 @@ public final class Const {
 
     
     public static final class Version {
-        public static final String NUMBER = "v1.0.0";
+        public static final String NUMBER = "v1.0.1";
     }
     
     public static final class PathName {
