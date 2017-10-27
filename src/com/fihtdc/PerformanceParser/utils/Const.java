b799d4aca9057caf2c7b52e9fb5b1233a8adff14
MinSMChien <MinSMChien@fih-foxconn.com>
2017-10-27 16:24:51 +0800

    Performance Parser Tool v1.0.4

--- src/com/fihtdc/PerformanceParser/utils/Const.java
+++ src/com/fihtdc/PerformanceParser/utils/Const.java
@@ -12,7 +12,7 @@ public final class Const {
 
     
     public static final class Version {
-        public static final String NUMBER = "v1.0.3";
+        public static final String NUMBER = "v1.0.4";
     }
     
     public static final class PathName {
